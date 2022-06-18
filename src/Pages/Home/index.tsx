2ddf9691e13f7f40a3990fd2b703a6dca8a9a348
ChandraRaditya <chandraariwiraditya@gmail.com>
2022-06-18 15:21:46 +0700

    add: make banner at home page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -2,8 +2,49 @@ import "./index.css";
 
 function Home() {
   return (
-    <div>
-      <h1>Hallo semuanya Home</h1>
+    <div className="banner">
+      <div className="banner-content-container">
+        <div className="cover-container">
+          <div className="cover-text-container">
+            <h1 className="cover-title">
+              Belajar Di DigDigCourse Dimanapun Dan Kapanpun
+            </h1>
+            <p className="cover-text">
+              DigDigCourse menyediakan berbagai pilihan Learning Path untuk
+              meningkatkan keterampilan Coding
+            </p>
+          </div>
+          <div className="cover-image">
+            <img src={require("../../Assets/Images/kid.png")} alt="" />
+          </div>
+        </div>
+        <div className="keuntungan-box">
+          <p className="keuntungan-title">Keuntungan Membeli Produk</p>
+          <div className="keuntungan-container">
+            <div className="keuntungan-item">
+              <p>
+                <span className="text-color">Koleksi</span> Lengkap{" "}
+                <span className="text-color">Learning Path</span>
+              </p>
+            </div>
+            <div className="keuntungan-item">
+              <p>
+                Mudah <span className="text-color">Dipelajari</span>
+              </p>
+            </div>
+            <div className="keuntungan-item">
+              <p>
+                Kurikulum <span className="text-color">Ter-update</span>
+              </p>
+            </div>
+            <div className="keuntungan-item">
+              <p>
+                <span className="text-color">Dipelajari</span> Darimana saja
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
